486ecdb82053d1c3450e16232d25eddf4ab96587
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-01-07 22:39:31 -0500

    added function to loop through current time and compare time of day in planner

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # UTA-Work-Day-Scheduler
 Challege 5 for UTA Bootcamp- Work Day Scheduler
+
+
+## Description:
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+User Story
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+Acceptance Criteria
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Mock-up](#mock-up)
+
+## Usage
+- Github page (https://github.com/mjtic/module2-portfolio/)
+- URL (https://mjtic.github.io/module2-portfolio/)
+
+![Horiseon-webpage](./assets/portfolio-module2.png)
+
+## Credits
+- As part of bootcamp module 2 challenged I have referenced sites shown below including mini-challenge 2 within UT-Austin bootcamp program:
+https://developer.mozilla.org
+
+
+## License
+
+Please refer to the LICENSE in the repo.
+
+
+
+## Mock-up##
+https://github.com/kpeazzoni/UTA-Work-Day-Scheduler/blob/main/assets/05-third-party-apis-homework-demo.gif
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,141 @@
 $( document ).ready(function() {
   console.log( "ready!" );
 });
+// TODO: Add code to display the current date in the header of the page.
+var currentDate = dayjs();
+currentDate.format('dddd, MMMM D YYYY, h:mm:ss a');
+var dateTime = $("#date-time");
+setInterval(displayTime, 1000);
+
+function displayTime() {
+    currentDate = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
+    dateTime.text(currentDate); 
+};
+
+var projectEl = $(".description")
+
+var saveEl = $(".saveBtn");
+var deleteEl;
+var userInputEl;
+var hourEl = $(".hour");
+var LastOfficeHour = 17; 
+
+var counter = 10; 
+setInterval(function() {  
+  counter++;
+}, 10000);
+//setInterval(compareTime, 3600000);
+setInterval(compareTime, 5000);
+
+function compareTime() { 
+  var currentHourEl = $("#hour-" + counter);
+  
+  if (currentHourEl != null && currentHourEl.hasClass('future')) {
+      currentHourEl.addClass('present');
+      currentHourEl.removeClass('future');
+
+      var pastHour = $("#hour-" + (counter - 1));
+      pastHour.addClass('past');
+      pastHour.removeClass('present');
+    } else if (currentHourEl > LastOfficeHour) {
+    $('.present').addClass('past');
+    $('.past').removeClass('present');
+  } 
+
+  if (counter == 00) {
+    $('.past').addClass('future');
+    $('.future').removeClass('past');
+  }
+}
+console.log(compareTime)
+// once it gets to 18, set 17 to past and next day change all to future
+// if (currentDate.hour().isBefore.currentHourEl) {
+//   document.getElementsByClassName("time-block").classList.add("past") 
+// } else if (currentDate.hour().isSame.currentHourEl)
+  // get the element by "hour-" + currentDate.hour and remove future class
+  // if element is found:
+  // then add 'present' class
+  // then get previous element, "hour-" + (currentDate.hour - 1), and replace present with past class
+
+  
+
+  // if (currentDate.hour().isBefore.officeHours[officeHoursIndex].hour) {
+  //   document.getElementById
+  //     .setAttribute("style", "background-color:gray");
+  // }else if (currentDate.hour().isSame.officeHours[officeHoursIndex].hour) {
+  //   projectEl
+  //         .setAttribute("style", "background-color:gray");
+  // } else (currentDate.hour().isAfter.officeHours[officeHoursIndex].hour) 
+  //   projectEl
+  //   .setAttribute("style", "background-color:green");
+// if (currentDate.hour() > officeHours[officeHoursIndex].hour) {
+//       projectEl
+//       .setAttribute("style", "background-color:gray");
+//   } else if (currentDate.hour() == officeHours[officeHoursIndex].hour) {
+//      projectEl
+//       .setAttribute("style", "background-color:red");
+//   } else (currentDate.hour() < officeHours[officeHoursIndex].hour) 
+//     projectEl
+//     .setAttribute("style", "background-color:green");
+//   };
+// console.log(currentDate.hour());
+
+
+// Reads projects from local storage and returns array of project objects.
+// Returns an empty array ([]) if there aren't any projects.
+
+// function readProjectsFromStorage() {
+//   var projects = localStorage.getItem('projects');
+//   if (projects) {
+//     projects = JSON.parse(projects);
+//   } else {
+//     projects = [];
+//   }
+//   return projects;
+// }
+
+// function saveProjectsToStorage(projects) {
+//   localStorage.setItem('projects', JSON.stringify(projects));
+// }
+
+// function printProjectData() {
+//   // clear current projects on the page
+//   projectDisplayEl.empty();
+
+//   for (var i = 0; i < projects.length; i += 1) {
+//     var project = projects[i];
+//     var projectDate = dayjs(project.date);
+//     // get date/time for start of today
+//     var today = dayjs().startOf('day');
+
+//     // Create row and columns for project
+//     var rowEl = $('<tr>');
+//     var nameEL = $('<td>').text(project.name);
+//     var typeEl = $('<td>').text(project.type);
+//     var dateEl = $('<td>').text(projectDate.format('MM/DD/YYYY'));
+
+//     // Save the index of the project as a data-* attribute on the button. This
+//   
+
+// will be used when removing the project from the array.
+//     var deleteEl = $(
+//       '<td><button class="btn btn-sm btn-delete-project" data-index="' +
+//         i +
+//         '">X</button></td>'
+//     );
+
+//     // add class to row by comparing project date to today's date
+//     if (projectDate.isBefore(today)) {
+//       rowEl.addClass('project-late');
+//     } else if (projectDate.isSame(today)) {
+//       rowEl.addClass('project-today');
+//     }
+
+//     // append elements to DOM to display them
+//     rowEl.append(nameEL, typeEl, dateEl, deleteEl);
+//     projectDisplayEl.append(rowEl);
+//   }
+// }
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -23,5 +158,46 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  
 });
+
+/* 
+var officeHours = [
+{
+  hour:"09",
+  id: "hour-9"
+},
+{
+  hour:"10",
+  id: "hour-10"
+},
+{
+  hour:"11",
+  id: "hour-11"
+},
+{
+  hour:"12",
+  id: "hour-12"
+},
+{
+  hour:"13",
+  id: "hour-13"
+},
+{
+  hour:"14",
+  id: "hour-14"
+},
+{
+  hour:"15",
+  id: "hour-15"
+},
+{
+  hour:"16",
+  id: "hour-16"
+},
+{
+  hour:"17",
+  id: "hour-17"
+},
+];
+*/
