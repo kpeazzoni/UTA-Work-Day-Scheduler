7dc15f3d124524f99a6b234217d1ff4f26002351
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-01-08 21:19:38 -0500

    added save to local storage stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,8 @@ function displayTime() {
     dateTime.text(currentDate); 
 };
 
-var projectEl = $(".description")
+var projectEl = $(".description").text();
+console.log(projectEl);
 
 var saveEl = $(".saveBtn");
 var deleteEl;
@@ -28,7 +29,7 @@ setInterval(function() {
   counter++;
 }, 10000);
 //setInterval(compareTime, 3600000);
-setInterval(compareTime, 5000);
+setInterval(compareTime, 10000);
 
 function compareTime() { 
   var currentHourEl = $("#hour-" + counter);
@@ -50,56 +51,73 @@ function compareTime() {
     $('.future').removeClass('past');
   }
 }
-console.log(compareTime)
-// once it gets to 18, set 17 to past and next day change all to future
-// if (currentDate.hour().isBefore.currentHourEl) {
-//   document.getElementsByClassName("time-block").classList.add("past") 
-// } else if (currentDate.hour().isSame.currentHourEl)
-  // get the element by "hour-" + currentDate.hour and remove future class
-  // if element is found:
-  // then add 'present' class
-  // then get previous element, "hour-" + (currentDate.hour - 1), and replace present with past class
+// console.log(currentDate)
 
+
+
+// TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function?
+  // How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+
+function handleProjectFormSubmit(event) {
+  event.preventDefault();
+};
+
+
+$(".saveBtn").on("click", function(event) {
+  //here is where I am using the 'this' hint and using the key and value in the local storage and writing it to the
+  //id='hour' to traverse the DOM!
+  var key = $(this).parent().attr("id").split("-")[1];
+  var value = $(this).parent().find(".description").val();
+  localStorage.setItem(key, value);
+  console.log(event);
+});
+
+ 
+function readProjectsFromStorage() {
+  localStorage.getItem(key, value);
   
+}
 
-  // if (currentDate.hour().isBefore.officeHours[officeHoursIndex].hour) {
-  //   document.getElementById
-  //     .setAttribute("style", "background-color:gray");
-  // }else if (currentDate.hour().isSame.officeHours[officeHoursIndex].hour) {
-  //   projectEl
-  //         .setAttribute("style", "background-color:gray");
-  // } else (currentDate.hour().isAfter.officeHours[officeHoursIndex].hour) 
-  //   projectEl
-  //   .setAttribute("style", "background-color:green");
-// if (currentDate.hour() > officeHours[officeHoursIndex].hour) {
-//       projectEl
-//       .setAttribute("style", "background-color:gray");
-//   } else if (currentDate.hour() == officeHours[officeHoursIndex].hour) {
-//      projectEl
-//       .setAttribute("style", "background-color:red");
-//   } else (currentDate.hour() < officeHours[officeHoursIndex].hour) 
-//     projectEl
-//     .setAttribute("style", "background-color:green");
-//   };
-// console.log(currentDate.hour());
 
+
+
+  // $( projectEl ).saveProjectsToStorage();
+
+
+// function saveProjects() {
+//   var item = $(".description").value;
+//   var getItem = localStorage.getItem("description");
+
+
+
+
+
+// console.log(projectEl)
 
 // Reads projects from local storage and returns array of project objects.
 // Returns an empty array ([]) if there aren't any projects.
+function readProjectsFromStorage() {
+  if (projectEl) {
+    projectEl = JSON.parse(projectEl);
+  } else {
+    projectEl = [];
+  }
+  return projectEl;
+}
 
-// function readProjectsFromStorage() {
-//   var projects = localStorage.getItem('projects');
-//   if (projects) {
-//     projects = JSON.parse(projects);
-//   } else {
-//     projects = [];
-//   }
-//   return projects;
-// }
 
-// function saveProjectsToStorage(projects) {
-//   localStorage.setItem('projects', JSON.stringify(projects));
-// }
+
+// once it gets to 18, set 17 to past and next day change all to future
+ 
+
+
+
+
 
 // function printProjectData() {
 //   // clear current projects on the page
@@ -117,7 +135,6 @@ console.log(compareTime)
 //     var typeEl = $('<td>').text(project.type);
 //     var dateEl = $('<td>').text(projectDate.format('MM/DD/YYYY'));
 
-//     // Save the index of the project as a data-* attribute on the button. This
 //   
 
 // will be used when removing the project from the array.
@@ -141,12 +158,7 @@ console.log(compareTime)
 // }
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -160,44 +172,3 @@ $(function () {
   //
   
 });
-
-/* 
-var officeHours = [
-{
-  hour:"09",
-  id: "hour-9"
-},
-{
-  hour:"10",
-  id: "hour-10"
-},
-{
-  hour:"11",
-  id: "hour-11"
-},
-{
-  hour:"12",
-  id: "hour-12"
-},
-{
-  hour:"13",
-  id: "hour-13"
-},
-{
-  hour:"14",
-  id: "hour-14"
-},
-{
-  hour:"15",
-  id: "hour-15"
-},
-{
-  hour:"16",
-  id: "hour-16"
-},
-{
-  hour:"17",
-  id: "hour-17"
-},
-];
-*/
