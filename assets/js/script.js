a95e83ecf47c077a3db036daf97e5de70895ed2a
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-01-09 10:32:19 -0500

    updated variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,37 +3,37 @@
 // in the html.
 $( document ).ready(function() {
   $('.time-block').each(function(i, obj) {
-    var id = $(this).attr('id');
-    var value = localStorage.getItem(id);
+    const id = $(this).attr('id');
+    const value = localStorage.getItem(id);
     $(this).find(".description").val(value);
   });
 });
 
 $(".saveBtn").on("click", function(event) {
-  var key = $(this).parent().attr("id");
-  var value = $(this).parent().find(".description").val();
+  const key = $(this).parent().attr("id");
+  const value = $(this).parent().find(".description").val();
   localStorage.setItem(key, value);
 });
 
-var dateTime = $("#date-time");
-var LastOfficeHour = 17; 
+const dateTime = $("#date-time");
+let LastOfficeHour = 17; 
 
 setInterval(compareTime, 3600000);
 setInterval(displayTime, 1000);
 
 function displayTime() {
-  var currentDateFormatted = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
+  const currentDateFormatted = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
   dateTime.text(currentDateFormatted); 
 }
 
 function compareTime() { 
-  var currentDate = dayjs();
-  var currentHourEl = $("#hour-" + currentDate.hour());
+  const currentDate = dayjs();
+  const currentHourEl = $("#hour-" + currentDate.hour());
   if (currentHourEl != null && currentHourEl.hasClass('future')) {
       currentHourEl.addClass('present');
       currentHourEl.removeClass('future');
 
-      var pastHour = $("#hour-" + (currentDate.hour() - 1));
+      const pastHour = $("#hour-" + (currentDate.hour() - 1));
       pastHour.addClass('past');
       pastHour.removeClass('present');
     } else if (currentHourEl > LastOfficeHour) {
